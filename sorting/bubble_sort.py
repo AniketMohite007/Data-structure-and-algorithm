079672f8a46418381bdb64233d503676aca69d79
Aniket Mohite <aniketmohite2022@gmail.com>
2026-02-18 23:14:54 +0530

    added bubble sort implementation

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble(nums):
+    n=len(nums)
+    for i in range(n):
+        for j in range(n-i-1):
+            if nums[j]>nums[j+1]:
+               nums[j],nums[j+1]=nums[j+1],nums[j]
+nums=[4,6,2,3,9,1,5]
+bubble(nums)
+print(nums)
+
