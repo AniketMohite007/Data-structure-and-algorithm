bfc88cae5b25d2f94f2d543469fc2e8d6e0471ab
Aniket Mohite <104272511+AniketMohite007@users.noreply.github.com>
2026-02-18 23:01:07 +0530

    Delete bubble_sort.py

--- bubble_sort.py
+++
@@ -1,9 +0,0 @@
-def bubble(nums):
-    n=len(nums)
-    for i in range(n):
-        for j in range(0,n-i-1):
-            if nums[j]>nums[j+1]:
-                nums[j],nums[j+1]=nums[j+1],nums[j]
-nums=[2,4,6,1,3,7,5]
-bubble(nums)
-print(nums)
